6f63c1c087a0ede378275aa2641dde2d99bc6133
Ishan <ishanwijes@gmail.com>
2022-08-02 20:42:27 +1000

    student activity 6 completed - added table and showed tables for particular database

--- 01-Activities/05-Ins_Tables/db/schema.sql
+++ 01-Activities/05-Ins_Tables/db/schema.sql
@@ -1,3 +1,5 @@
+
+-- before you create a databases, always check to see if it already exists
 DROP DATABASE IF EXISTS inventory_db;
 -- Creates the "inventory_db" database --
 CREATE DATABASE inventory_db;
@@ -5,11 +7,13 @@ CREATE DATABASE inventory_db;
 -- Makes it so all of the following code will affect inventory_db --
 USE inventory_db;
 
--- Creates the table "produce" within inventory_db --
-CREATE TABLE produce (
+-- Creates the table "product" within inventory_db --
+CREATE TABLE product (
   -- Creates a numeric column called "id" which will automatically increment its default value as we create new rows --
   id INT NOT NULL,
   -- Makes a string column called "name" which cannot contain null --
-  name VARCHAR(100) NOT NULL
+  firstName VARCHAR(100) NOT NULL
+
+  description VARCHAIN(500) NULL
 );
 
