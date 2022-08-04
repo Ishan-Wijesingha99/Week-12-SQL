51ee8977cada7116a7bad0a2f72477cd9d693de1
Ishan <ishanwijes@gmail.com>
2022-08-04 21:09:59 +1000

    student activity 18 completed - seeds, auto incrementing table value, primary keys

--- 01-Activities/18-Stu_Seeds/Unsolved/db/schema.sql
+++ 01-Activities/18-Stu_Seeds/Unsolved/db/schema.sql
@@ -4,7 +4,9 @@ CREATE DATABASE grocery_db;
 USE grocery_db;
 
 CREATE TABLE products(
-  id INT NOT NULL,
+  -- whatever is incremented, must also be labelled as the primary key
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  
   product_name VARCHAR(30) NOT NULL,
   category_name VARCHAR(30) NOT NULL
 );
--- 01-Activities/18-Stu_Seeds/Unsolved/db/seeds.sql
+++ 01-Activities/18-Stu_Seeds/Unsolved/db/seeds.sql
@@ -1,3 +1,6 @@
+
+-- here we are inserting data into the products table, but we are leaving out the id
+-- we can do this because we already specified in schema.sql that id would have it's value incremented (the default starting value of this increment is 1, and it is always incremented by +1)
 INSERT INTO products (product_name, category_name)
 VALUES ("spinach", "produce"),
        ("peanut butter", "staples"),
