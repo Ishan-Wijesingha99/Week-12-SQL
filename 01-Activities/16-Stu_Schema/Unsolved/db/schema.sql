45f609f07d25ab3447406c6db1e047f4ac9a7b3a
Ishan <ishanwijes@gmail.com>
2022-08-04 20:42:22 +1000

    student activity 16 completed - data types for tables exercise

--- 01-Activities/16-Stu_Schema/Unsolved/db/schema.sql
+++ 01-Activities/16-Stu_Schema/Unsolved/db/schema.sql
@@ -1,1 +1,19 @@
 -- Write your schema here --
+DROP DATABASE IF EXISTS productsCategories_db;
+CREATE DATABASE productsCategories_db;
+
+USE productsCategories_db;
+
+CREATE TABLE products (
+  id INT NOT NULL,
+  product_name VARCHAR(30) NOT NULL,
+  category_name VARCHAR(100) NOT NULL,
+  price INT NOT NULL,
+  in_stock BOOLEAN NOT NULL
+);
+
+CREATE TABLE categories (
+  id INT NOT NULL,
+  category_name VARCHAR(30) NOT NULL,
+);
+
