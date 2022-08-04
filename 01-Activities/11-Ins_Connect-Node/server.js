8c88bcd12916c0268807067d3aa7b3b911094109
Ishan <ishanwijes@gmail.com>
2022-08-04 19:47:02 +1000

    student activity 12 completed - connecting database to node

--- 01-Activities/11-Ins_Connect-Node/server.js
+++ 01-Activities/11-Ins_Connect-Node/server.js
@@ -1,37 +1,55 @@
 const express = require('express');
-// Import and require mysql2
+
+// Import and require mysql2 to execute mysql database commands from node
 const mysql = require('mysql2');
 
+// we define a port variable like this so that, when we deploy on Heroku, the port will be process.env.PORT, and when we run the server on our local device, it will be 3001
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Connect to database
+// need to store mysql.createConnection() in variable, often called db
+// this will be an object
 const db = mysql.createConnection(
+  // need to give information about the database connection we want to create
   {
     host: 'localhost',
-    // MySQL username,
     user: 'root',
-    // MySQL password
-    password: '',
+    password: 'mysqlishan99',
     database: 'classlist_db'
   },
+
   console.log(`Connected to the classlist_db database.`)
 );
 
-// Query database
-db.query('SELECT * FROM students', function (err, results) {
+
+
+// you need to write 'source db/seeds.sql' so that node knows where to get the sql file
+db.query('source db/seeds.sql', (err, results) => {
   console.log(results);
+
+  // use db.query to write database commands from node
+  // first argument is the database command
+  // second argument is a callback function that is executed once this command is completed
+  db.query('SELECT * FROM students;', (err, results) => {
+
+  })
+
 });
 
-// Default response for any other request (Not Found)
+
+
 app.use((req, res) => {
   res.status(404).end();
 });
 
+
+
+// listening to port
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
--- 01-Activities/12-Stu_Connect-Node/Unsolved/server.js
+++ 01-Activities/12-Stu_Connect-Node/Unsolved/server.js
@@ -1,30 +1,56 @@
 const express = require('express');
+
+// we need this package to run database commands FROM NODE
 const mysql = require('mysql2');
 
+// we define a port variable like this so that, when we deploy on Heroku, the port will be process.env.PORT, and when we run the server on our local device, it will be 3001
 const PORT = process.env.PORT || 3001;
+
 const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+
+
+// need to store mysql.createConnection() in variable, often called db
+// this will be an object
 const db = mysql.createConnection(
+  // need to give information about the database connection we want to create
   {
     host: 'localhost',
     user: 'root',
-    password: '',
+    password: 'mysqlishan99',
     database: 'classlist_db'
   },
+
   console.log(`Connected to the classlist_db database.`)
 );
 
-db.query('SELECT * FROM students', function (err, results) {
+
+
+// you need to write 'source db/seeds.sql' so that node knows where to get the sql file
+db.query('source db/seeds.sql', (err, results) => {
   console.log(results);
+
+  // use db.query to write database commands from node
+  // first argument is the database command
+  // second argument is a callback function that is executed once this command is completed
+  db.query('SELECT * FROM students;', (err, results) => {
+
+  })
+
 });
 
-app.use((req, res) => {
+
+
+app.use((req, res, next) => {
   res.status(404).end();
 });
 
+
+
+// listening to port
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
